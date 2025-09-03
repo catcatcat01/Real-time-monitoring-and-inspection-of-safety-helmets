ab4f22f989b270423cffe4598699dce43b9f7a54
catcatcat01 <2474821667@qq.com>
2025-09-03 12:08:48 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,11 +6,11 @@ load_dotenv()
 
 class Config:
     # 数据库配置
-    DB_HOST = os.getenv('DB_HOST', '10.66.102.120')
-    DB_PORT = os.getenv('DB_PORT', '5432')
-    DB_USER = os.getenv('DB_USER', 'postgres')
-    DB_PASSWORD = os.getenv('DB_PASSWORD', '123456')
-    DB_NAME = os.getenv('DB_NAME', 'fastgate')
+    DB_HOST = os.getenv('DB_HOST', '')
+    DB_PORT = os.getenv('DB_PORT', '')
+    DB_USER = os.getenv('DB_USER', '')
+    DB_PASSWORD = os.getenv('DB_PASSWORD', '')
+    DB_NAME = os.getenv('DB_NAME', '')
 
     # 应用配置
     UPLOAD_FOLDER = os.path.join('app', 'static', 'uploads')
@@ -25,31 +25,32 @@ class Config:
     FACE_REC_MODEL_PATH = os.path.join('models', 'dlib_face_recognition_resnet_model_v1.dat')
 
     # 实时监控m3u8地址（用户提供的链接）
-    MONITOR_M3U8_URL = "https://open.ys7.com/v3/openlive/FK9284610_1_1.m3u8?expire=1786764266&id=878238042844680192&t=14b64c503c51412bf12bb0ac93bbe423ff302e6f196a81587d25836c7eb689ab&ev=100"
+    MONITOR_M3U8_URL = ""
     # 监控区域名称（用于告警标识）
-    MONITOR_LOCATION = "M4-1F-化成"
+    MONITOR_LOCATION = ""
 
     # 确保目录存在
     os.makedirs(UPLOAD_FOLDER, exist_ok=True)
     os.makedirs(RESULTS_FOLDER, exist_ok=True)
 
     # -------------------------- 超脑设备1配置 --------------------------
-    SUPERBRAIN1_IP = "10.60.201.249"  # 设备1IP
-    SUPERBRAIN1_PORT = 8000  # 设备1端口（默认8000）
-    SUPERBRAIN1_USER = "admin"  # 设备1用户名
-    SUPERBRAIN1_PWD = "waffer248"  # 设备1密码
+    SUPERBRAIN1_IP = ""  # 设备1IP
+    SUPERBRAIN1_PORT =   # 设备1端口（默认8000）
+    SUPERBRAIN1_USER = ""  # 设备1用户名
+    SUPERBRAIN1_PWD = ""  # 设备1密码
     SUPERBRAIN1_SDK_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'lib')  # 设备1SDK路径
     SUPERBRAIN1_LOG_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'logs', 'superbrain1')  # 设备1日志路径
-    SUPERBRAIN1_MONITOR_LOCATION = "WIM超脑监控"  # 设备1监控区域名
+    SUPERBRAIN1_MONITOR_LOCATION = ""  # 设备1监控区域名
 
     # -------------------------- 超脑设备2配置 --------------------------
-    SUPERBRAIN2_IP = "10.62.3.222"  # 设备2IP（替换为实际IP）
-    SUPERBRAIN2_PORT = 8000  # 设备2端口
-    SUPERBRAIN2_USER = "admin"  # 设备2用户名
-    SUPERBRAIN2_PWD = "waffer12"  # 设备2密码（替换为实际密码）
+    SUPERBRAIN2_IP = ""  # 设备2IP（替换为实际IP）
+    SUPERBRAIN2_PORT =   # 设备2端口
+    SUPERBRAIN2_USER = ""  # 设备2用户名
+    SUPERBRAIN2_PWD = ""  # 设备2密码（替换为实际密码）
     SUPERBRAIN2_SDK_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'lib')  # 设备2SDK路径（可与设备1共用）
     SUPERBRAIN2_LOG_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'logs', 'superbrain2')  # 设备2日志路径
-    SUPERBRAIN2_MONITOR_LOCATION = "WIH超脑监控"  # 设备2监控区域名
+    SUPERBRAIN2_MONITOR_LOCATION = ""  # 设备2监控区域名
 
     os.makedirs(SUPERBRAIN1_LOG_PATH, exist_ok=True)
+
     os.makedirs(SUPERBRAIN2_LOG_PATH, exist_ok=True)
